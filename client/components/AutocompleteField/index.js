20437752087601c7ffb0e98398166d3ff4b69ff0
Michael Mazurczak <maz-dev@lagavulin.local>
2016-03-07 17:28:14 +0100

    Cleanup and Autocomplete with object

--- client/components/AutocompleteField/index.js
+++ client/components/AutocompleteField/index.js
@@ -4,66 +4,52 @@ import { connect } from 'react-redux';
 import {Typeahead, Tokenizer} from 'react-typeahead';
 import style from './style.css';
 
-function getEmails() {
-  return [
-      'andy@protonmail.com',
-      'jason@protonmail.com',
-      'richard@protonmail.com',
-      'nick@protonmail.com',
-      'nicole@protonmail.com',
-      'charlie@protonmail.com',
-      'tango@protonmail.com',
-  ];
+var emailsList = [
+     {label: 'andy', value:'andy@protonmail.com'},
+     {label: 'jason', value:'jason@protonmail.com'},
+     {label: 'richard', value:'richard@protonmail.com'},
+     {label: 'nick', value:'nick@protonmail.com'},
+     {label: 'nicole', value:'nicole@protonmail.com'},
+     {label: 'charlie', value:'charlie@protonmail.com'},
+     {label: 'tango', value:'tango@protonmail.com'}
+ ];
+
+
+function extractMail(mail) {
+  var EmailRegex = /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/gi;
+  //"""
+  return EmailRegex.exec(mail);
 }
-
-let styles = {
-  item: {
-    padding: '6px 10px',
-    cursor: 'default'
-  },
-
-  highlightedItem: {
-    color: 'white',
-    background: 'hsl(200, 50%, 50%)',
-    padding: '6px 10px',
-    cursor: 'default'
-  },
-
-  menu: {
-    border: 'solid 1px #ccc'
-  }
-};
 export default class AutocompleteField extends Component {
   constructor() {
     super()
     this.state = { badges: [] };
   }
 
-  select(value) {
-    // TODO: This part should be handled by flux actions.
-    console.log(this.state);
-    var oldBadges = this.state.badges;
-    var nextBadges = oldBadges.push(value);
-    this.setState({badges: nextBadges});
-  }
-
+  // TODO: This part should be handled by flux actions.
+  //regex email: .constant("regexEmail", /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/gi)
   render() {
-    // this.state.badges.map(function(badge, index) {
-    //   console.log("badge!!!!" + badge);
-    //   console.log("index!!!!" + index);
-    // });
     return (
       <div className={style.autocomplete}>
         <span>to:</span>
           <Tokenizer
             className={style.typeahead}
-            options={getEmails()}
+            options={emailsList}
             onTokenAdd={function(token) {
+              emailList.push({label: "", value: token});
               console.log('token added: ', token);
             }}
             allowCustomValues={1}
-            displayOption={function(option, index) {
-              return option;
+            displayOption={function(option) {
+              if(option.label) {
+                return option.label + ' <' + option.value + '>';
+              } else {
+                return option;
+              }
+
+            }}
+            filterOption={function(options) {
+              return {label: "", value: options};
             }}
           />
 
